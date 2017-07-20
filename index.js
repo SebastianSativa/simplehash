39dcda946411f3bc55a48b0133004adc82681d15
Sebastian Sativa <sativa.sebastian@gmail.com>
2017-07-20 04:31:09 +0200

    Fixed syntax errors

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const XXHash = require('xxhash');
 const SEED = 0xCAFEBABE;
 
 module.exports = function(string) {
-	return XXHash.hash(Buffer.from(String(string), SEED));
+	return XXHash.hash(Buffer.from(String(string).toString(), 'utf8'), SEED);
 }
